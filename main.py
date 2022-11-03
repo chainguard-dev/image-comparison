c270ab1c8ec4505c4166341470050521bae9c308
John Speed Meyers <jsmeyers@chainguard.dev>
2022-11-03 13:44:24 -0400

    Add retrieval of GH secret

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 """Prepare image CVE data for publishing publicly."""
 
+import os
+
 import pandas as pd
 
 from filter_data import filter_df
 
+# Details about bucket from which to download images CVE data
+BUCKET = os.environ.get("BUCKET")
+
 if __name__ == "__main__":
-    df = pd.read_csv("")
+    df = pd.read_csv(f"gs://{BUCKET}/latest.csv")
     filtered_df = filter_df(df)
     filtered_df.to_csv("data.csv", parse_dates=["time"])
     # TODO: Commit and push to main
