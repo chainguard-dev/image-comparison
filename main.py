7c9af1ed00fe34703fd1f934c7191794fc02a656
John Speed Meyers <jsmeyers@chainguard.dev>
2022-11-03 11:40:26 -0400

    Add main file

---
+++ main.py
@@ -0,0 +1,11 @@
+"""Prepare image CVE data for publishing publicly."""
+
+import pandas as pd
+
+from filter_data import filter_df
+
+if __name__ == "__main__":
+    df = pd.read_csv("")
+    filtered_df = filter_df(df)
+    filtered_df.to_csv("data.csv")
+    # TODO: Commit and push to main
