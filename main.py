53539301e8fbb4d23cf455c7532b7afd52429579
John Speed Meyers <jsmeyers@chainguard.dev>
2022-11-03 19:38:53 -0400

    Use data URL rather than bucket

--- main.py
+++ main.py
@@ -6,11 +6,9 @@ import pandas as pd
 
 from filter_data import filter_df
 
-# Details about bucket from which to download images CVE data
-BUCKET = os.environ.get("BUCKET")
 
 if __name__ == "__main__":
-    df = pd.read_csv(f"gs://{BUCKET}/latest.csv")
+    df = pd.read_csv(os.environ.get("DATA_URL"))
     filtered_df = filter_df(df)
     filtered_df.to_csv("data.csv", parse_dates=["time"])
     # TODO: Commit and push to main
