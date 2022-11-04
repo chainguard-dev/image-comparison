2e17e4a36ff90f473091721f2bbfb0542637c7dd
John Speed Meyers <jsmeyers@chainguard.dev>
2022-11-04 09:26:19 -0400

    Parse date when ingesting, not exporting

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from filter_data import filter_df
 
 
 if __name__ == "__main__":
-    df = pd.read_csv(os.environ.get("DATA_URL"))
+    df = pd.read_csv(os.environ.get("DATA_URL"), parse_dates=["time"])
     filtered_df = filter_df(df)
-    filtered_df.to_csv("data.csv", parse_dates=["time"])
+    filtered_df.to_csv("data.csv")
     # pylint: disable=fixme
     # TODO: Commit and push to main
