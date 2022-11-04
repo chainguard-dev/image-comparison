a77f82bf382434ea27b30d433e8e12bc5d593284
John Speed Meyers <jsmeyers@chainguard.dev>
2022-11-04 09:32:05 -0400

    Separate environment variable into own line

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ import pandas as pd  # pylint: disable=import-error
 
 from filter_data import filter_df
 
+DATA_URL = os.environ.get("DATA_URL")
 
 if __name__ == "__main__":
-    df = pd.read_csv(os.environ.get("DATA_URL"), parse_dates=["time"])
+    df = pd.read_csv(DATA_URL, parse_dates=["time"])
     filtered_df = filter_df(df)
     filtered_df.to_csv("data.csv")
     # pylint: disable=fixme
