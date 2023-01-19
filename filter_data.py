dc2ad0d58b51609b4071d3d8a5c49ba62629977d
John Speed Meyers <jsmeyers@chainguard.dev>
2023-01-19 15:42:41 -0500

    Drop negligible CVE count column too
    I would actually like to this column, but as an experiment, I want to see if dropping this column leads to a working graph.

--- filter_data.py
+++ filter_data.py
@@ -63,7 +63,7 @@ def filter_df(dataframe, starting_day=None, ending_day=None):
 
     # drop "success" column since that is only interesting for
     # internal chainguard quality control purposes
-    filtered_df = filtered_df.drop(columns=["success"])
+    filtered_df = filtered_df.drop(columns=["success", "negligible_cve_cnt"])
 
     # reset index (done to enable reproducibility during testing)
     filtered_df = filtered_df.reset_index(drop=True)
